ec8b82b28ff37859aee7020348b5b108f2c0f16f
Alexey Blinov <alexey.blinov@gmail.com>
2013-08-30 18:34:57 +0100

    Removed test code

--- Audjustable/Classes/AudioPlayer/AudioPlayer.m
+++ Audjustable/Classes/AudioPlayer/AudioPlayer.m
@@ -961,7 +961,7 @@ static void AudioQueueIsRunningCallbackProc(void* userData, AudioQueueRef audioQ
         }
     }
     
-    if (signal || YES)
+    if (signal)
     {
         pthread_cond_signal(&queueBufferReadyCondition);
     }
