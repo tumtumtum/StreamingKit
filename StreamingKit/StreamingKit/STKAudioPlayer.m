5d68ab0c584b677a6ad0409878b688a197fc9f60
Андрей <andrej@MacBook-Pro-Andrej.local>
2014-06-05 22:34:10 +0400

    add ipod-library loader

--- StreamingKit/StreamingKit/STKAudioPlayer.m
+++ StreamingKit/StreamingKit/STKAudioPlayer.m
@@ -37,6 +37,7 @@
 #import "STKHTTPDataSource.h"
 #import "STKAutoRecoveringHTTPDataSource.h"
 #import "STKLocalFileDataSource.h"
+#import "STKMediaLibraryFileLoader.h"
 #import "STKQueueEntry.h"
 #import "NSMutableArray+STKAudioPlayer.h"
 #import "libkern/OSAtomic.h"
@@ -624,6 +625,9 @@ static void AudioFileStreamPacketsProc(void* clientData, UInt32 numberBytes, UIn
     else if ([url.scheme caseInsensitiveCompare:@"http"] == NSOrderedSame || [url.scheme caseInsensitiveCompare:@"https"] == NSOrderedSame)
     {
         retval = [[STKAutoRecoveringHTTPDataSource alloc] initWithHTTPDataSource:[[STKHTTPDataSource alloc] initWithURL:url]];
+    }
+    else if ([url.scheme caseInsensitiveCompare:@"ipod-library"] == NSOrderedSame) {
+        retval = [STKMediaLibraryFileLoader localFileDataSourceWithMediaLibraryURL:url];
     }
     
     return retval;
