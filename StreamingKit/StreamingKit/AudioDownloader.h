5534354a94506ca4c56789acfd76c98003258db2
Gary Tartt <TreyTartt@Garys-MBP.home>
2015-02-15 02:07:12 -0600

    Added support for m3u and m3u8 files

---
+++ StreamingKit/StreamingKit/AudioDownloader.h
@@ -0,0 +1,31 @@
+//
+//  AudioDownloader.h
+//  Radio
+//
+//  Created by Trey Tartt on 8/9/14.
+//  Copyright (c) 2014 Trey Tartt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "STKAudioPlayer.h"
+
+@interface AudioDownloader : NSObject
+@property (nonatomic, retain) NSMutableArray *addedAudioPackets;
+@property (nonatomic, retain) NSURLSessionDataTask *dataTask;
++ (id)AudioDownloaderItem;
+
+
+- (void)clearData;
+
+/*
+   creates session and fetches the aac files
+   from that session url
+ */
+- (void)createM3u8Session:(NSString *)url;
+
+/*
+   get the url to stream from the m3u file
+   and queue to audio player
+ */
+- (void)getM3uURLAndPlay:(NSString *)url;
+@end
